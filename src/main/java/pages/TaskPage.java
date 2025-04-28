e3093c17aa36a204af4ca2593bcfc39e0cb0af7f
aleks-chubarova21 <chubarovasasha32@gmail.com>
2025-04-28 21:06:05 +0300

    Commit

--- src/main/java/pages/TaskPage.java
+++ src/main/java/pages/TaskPage.java
@@ -9,9 +9,6 @@ import static com.codeborne.selenide.Condition.*;
 
 public class TaskPage {
     private final SelenideElement statusField = $x("//span[@id='status-val']");
-    private final SelenideElement transitionButton = $x("//span[contains(@class,'jira-workflow-transition')]");
-    private final SelenideElement transitionDialog = $x("//div[@role='dialog']");
-    private final SelenideElement submitTransitionButton = $x("//input[@type='submit']");
     private final SelenideElement currentStatus = $x("//span[@id='status-val']/span");
     private final SelenideElement businessProcessButton = $x("//span[text()='Бизнес-процесс']");
     private final SelenideElement inProgressButton = $x("//span[text()='В работе']/ancestor::a");
@@ -24,21 +21,19 @@ public class TaskPage {
     
     public void clickInProgressButton() {
         inProgressButton.shouldBe(visible, enabled).click();
-        sleep(2000); // Ждем обновления статуса
+        sleep(2000);
     }
     
     public void clickBusinessProcessAndDone() {
-        // Нажимаем на кнопку Бизнес-процесс
+
         businessProcessButton.shouldBe(visible, enabled).click();
         sleep(1000);
-        
-        // Нажимаем на кнопку Выполнено
+
         doneButton.shouldBe(visible, enabled).click();
-        sleep(2000); // Ждем обновления статуса
+        sleep(2000);
     }
-    
     public boolean verifyCurrentStatus(String expectedStatus) {
-        sleep(1000); // Ждем обновления статуса
+        sleep(1000);
         return currentStatus.shouldBe(visible).getText().equals(expectedStatus);
     }
 }
