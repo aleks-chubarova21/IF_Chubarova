05a4d7691febae143b324f894627642c42b5eb63
aleks-chubarova21 <chubarovasasha32@gmail.com>
2025-06-03 23:00:12 +0300

    Commit

--- src/main/java/pages/AuthPage.java
+++ src/main/java/pages/AuthPage.java
@@ -3,31 +3,60 @@ package pages;
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.SelenideElement;
 import io.qameta.allure.Step;
+
 import static com.codeborne.selenide.Selenide.$x;
-import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Condition.visible;
 
 public class AuthPage {
     private final SelenideElement usernameInput = $x("//input[@id='login-form-username']");
     private final SelenideElement passwordInput = $x("//input[@id='login-form-password']");
     private final SelenideElement loginButton = $x("//input[@id='login-form-submit']");
     private final SelenideElement userProfileIcon = $x("//a[@id='header-details-user-fullname']");
-    private final SelenideElement errorMessage = $x("//div[contains(@class, 'aui-message-error')]");
 
-    @Step("Открытие страницы авторизации: {url}")
-    public void open(String url) {
+    @Step("Открытие страницы авторизации по URL: {url}")
+    public void open (String url) {
         Selenide.open(url);
     }
 
-    @Step("Авторизация пользователя: {username}")
+    @Step("Выполнение авторизации пользователя: {username}")
     public void login(String username, String password) {
-        usernameInput.shouldBe(visible, interactable).setValue(username);
-        passwordInput.shouldBe(visible, interactable).setValue(password);
-        loginButton.shouldBe(visible, interactable).click();
-        userProfileIcon.shouldBe(visible);
+        inputUsername(username);
+        inputPassword(password);
+        submitLogin();
+        verifyLoginSuccess();
     }
 
-    @Step("Проверка авторизации пользователя")
+    @Step("Ввод логина")
+    private void inputUsername(String username) {
+        usernameInput
+                .shouldBe(visible)
+                .setValue(username);
+    }
+
+    @Step("Ввод пароля")
+    private void inputPassword(String password) {
+        passwordInput
+                .shouldBe(visible)
+                .setValue(password)
+                .sensitive();
+    }
+
+    @Step("Нажатие кнопки входа")
+    private void submitLogin() {
+        loginButton
+                .shouldBe(visible)
+                .click();
+    }
+
+    @Step("Проверка успешной авторизации")
     public boolean isUserLoggedIn() {
-        return userProfileIcon.isDisplayed();
+        return userProfileIcon
+                .shouldBe(visible)
+                .isDisplayed();
+    }
+
+    @Step("Подтверждение успешного входа")
+    private void verifyLoginSuccess() {
+        userProfileIcon.shouldBe(visible);
     }
 }
